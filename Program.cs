1cc5afa467b3b556c4012ac5328e6f1dde5b29c7
jhdev3 <jacobheden@gmail.com>
2021-11-03 12:21:20 +0100

    Working calc with all criteras
    Cant quit in the check_input loop but otherwise working and history writes when exiting program

--- Program.cs
+++ Program.cs
@@ -1,77 +1,104 @@
 ﻿using System;
+using System.Collections.Generic; //List
+
 namespace Calculator
 {
     class Program
     {
+        // TryParse framför Pars TryParse returnar 0 om det misslyckas istället för Excetions så gör det enklare att hantera och gå vidare med TryParse
+        private static float check_input_float(string input) //Gör om string till float. 
+        {
+                bool check = true;
+                float f;
+
+                do
+                {
+                    check = float.TryParse(input, out f);
+
+                    if (check == false)
+                    {
+                        Console.WriteLine("Felaktigt inmating, Mata in ett talet på nytt tack :)");  // Cant Quit program from here, not the point either. 
+                        input = Console.ReadLine();
+                    }
+
+                } while (!check); //Same as the if-statment above, just using logical operators.  
+            
+                return f;
+           }
+           
         static void Main(string[] args)
         {
-            float tal1=0, tal2=0; //Lägga som private/public i klass kanske
-            string calc_operator;
-            bool run = true;
+            float tal1=0, tal2=0, resultat; //Lägga som private/public i klass kanske för OOP standard = 0
+            string read;
+            string calc_operator = "+";
+            List<string>  history = new List<string>(); //Skapar en array/lista/vector
 
+            Console.WriteLine("En fantatiskt miniräknare, där om du vill AVSLUTA matar in MARKUS \n Mata in ett tal:");
 
-           
             do
             {
-                Console.WriteLine("Mata in ett tal: \n");
-                tal1 = float.Parse(Console.ReadLine()); //Gör om string till float. 
-                calc_operator = Console.ReadLine();
-                tal2 = float.Parse(Console.ReadLine());
+
+                read = Console.ReadLine();
+                if (read == "MARCUS")
+                {
+                    break;
+                }
+                tal1 = check_input_float(read);
+
+                read = Console.ReadLine();
+                if (read == "MARCUS")
+                {
+                    break;
+                }
+                calc_operator = read;
+                read = Console.ReadLine();
+                if (read =="MARCUS") {
+                    break;
+                }
+                tal2 = check_input_float(read);
 
                 switch (calc_operator)
                 {
                     case "+":
-                        Console.WriteLine("{0} + {1} = {2} ", tal1, tal2, tal1 + tal2);
+                        resultat = tal1 + tal2;
+                        Console.WriteLine("{0} + {1} = {2} ", tal1, tal2, resultat);
+                        history.Add(tal1+ calc_operator + tal2 + " = " + resultat);
                         break;
                     case "-":
-                        Console.WriteLine("{0} - {1} = {2} ", tal1, tal2, tal1 - tal2);
+                        resultat = tal1 - tal2;
+                        Console.WriteLine("{0} - {1} = {2} ", tal1, tal2, resultat);
+                        history.Add(tal1 + calc_operator + tal2 + " = " + resultat);
                         break;
                     case "*":
-                        Console.WriteLine("{0} * {1} = {2} ", tal1, tal2, tal1 * tal2);
+                        resultat = tal1 * tal2;
+                        Console.WriteLine("{0} * {1} = {2} ", tal1, tal2, resultat);
+                        history.Add(tal1 + calc_operator + tal2 + " = " + resultat);
                         break;
                     case "/":
-                        Console.WriteLine("{0} / {1} = {2} ", tal1, tal2, tal1 / tal2);
+                        resultat = tal1 / tal2;
+                        Console.WriteLine("{0} / {1} = {2} ", tal1, tal2, resultat);
+                        history.Add(tal1 + calc_operator + tal2 + " = " + resultat);
                         break;
                     default:
-                        Console.WriteLine("Felaktig inmanting din dummer");
-                        run = false;
+                        Console.WriteLine("Använd lämplig operator din dummer");                      
                         break;
 
                 }
-            } while (run);
-
-
+            } while (read != "MARCUS");
+           
+            Console.WriteLine("Kalkyl Historik: ");
+            foreach (string calc in history)
+            {
+                Console.WriteLine(calc);
+            }
+            Console.WriteLine("Hej då");
+            
             Console.ReadKey();
         }
+
+       
     }
+
 }
 
 
-/* 
- 
-Good to know: 
-
-Float - 7 digits (32 bit)
-
-Double-15-16 digits (64 bit)
-
-Decimal -28-29 significant digits (128 bit)
- 
-
-Metoder: 
- public, protected eller private = OOP  kan sättas framör static, till console applications.  
-
-Lamba metod = kort function/metod    func("inparametrar) x, y => "vad de utför" 
-
-Var = typ variabel skapa en local variabel utan att ge dem en explicit typ, String, int, double etc. 
-
-            
-            exempel på writeline. 
-            Console.WriteLine("Hello World!" + (tal1,  tal2));
-
-            Console.WriteLine("Hello World!\t{1} och {2} är totalt {0} ", tal1 + tal2, tal1, tal2 );
-
-
-        $ dotnet new gitignore <-- Grymt nice :) 
-
- */ 
